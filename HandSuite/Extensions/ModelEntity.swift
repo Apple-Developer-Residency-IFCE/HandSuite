b74c26de85ac078edf94e705e10e6cb9c56b6f9c
NarelyLima <narely148@gmail.com>
2025-07-31 20:55:21 -0300

    Adicionada função para criar uma animacao e uma funcao para remover uma animacao

--- HandSuite/Extensions/ModelEntity.swift
+++ HandSuite/Extensions/ModelEntity.swift
@@ -13,6 +13,62 @@ public extension ModelEntity {
         let simpleMaterial = SimpleMaterial(color: UIColor(hex: hexColor), isMetallic: false)
         return ModelEntity(mesh: .generateSphere(radius: radius), materials: [simpleMaterial])
     }
+    
+    static private func animationDuration(for entity: Entity) -> TimeInterval? {
+        entity.availableAnimations.first?.definition.duration
+    }
+    
+    static func createAnimation(
+        to content: RealityViewContent,
+        name: String,
+        bundle: Bundle,
+        completion: @escaping (_ entity: Entity, _ anchor: AnchorEntity) -> Void
+    ) {
+        Task {
+            do {
+                let entity = try await Entity(named: name, in: bundle)
+                let anchor = AnchorEntity(.hand(.right, location: .palm))
+
+                entity.scale = SIMD3<Float>(repeating: 0.05)
+                anchor.addChild(entity)
+
+                await MainActor.run {
+                    content.add(anchor)
+                }
+
+                if let animation = entity.availableAnimations.first {
+                    // TODO: Create a Transform rotation function
+                    entity.playAnimation(animation, transitionDuration: 0.1)
+                }
+
+                completion(entity, anchor)
+
+            } catch {
+                print("Erro ao carregar ou manipular animação: \(error)")
+            }
+        }
+    }
+    
+    static func removeAnimation(from content: RealityViewContent, entity: Entity?, anchor: AnchorEntity?, completion: @escaping (_ removed: Bool) -> Void) {
+        Task {
+            var removed: Bool = false
+            if let previousEntity = entity,
+               let animDuration = animationDuration(for: previousEntity) {
+                try? await Task.sleep(nanoseconds: UInt64(animDuration * 1_000_000_000))
+            }
+
+            await MainActor.run {
+                if let anchor {
+                    content.remove(anchor)
+                    anchor.removeFromParent()
+                }
+                removed = true
+                
+            }
+            
+            completion(removed)
+        }
+    }
 }
 
 extension SIMD3: Sendable {}
